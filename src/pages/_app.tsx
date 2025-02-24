bef2cb89752d9d2c3ffbea677e81b03eb6d1d6cb
juniorbarbsa <60676892+juniorbarbsa@users.noreply.github.com>
2025-02-24 08:07:58 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redireciona para a página de login se a rota atual for a raiz
+    if (router.pathname === '/') {
+      router.push('/login');
+    }
+  }, [router]);
+
+  useEffect(() => {
+    // Redireciona para a ficha pré-anestésica em branco após login
+    if (router.pathname === '/login' && pageProps.isLoggedIn) {
+      sessionStorage.setItem('isNewLogin', 'true'); // Set flag for new login
+      router.push('/new-patient-form');
+    }
+  }, [router, pageProps.isLoggedIn]);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
